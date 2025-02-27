33d90b69751f160e37834b5e5259c5bcfb790c3b
Agustin Bollati <agustin.bollati@fluxit.com.ar>
2025-02-27 18:28:28 -0300

    feat: agregar página con la lista de issues y una issue en particular

---
+++ src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'issues-list-page',
+  imports: [],
+  templateUrl: './issues-list-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class IssuesListPageComponent {}
