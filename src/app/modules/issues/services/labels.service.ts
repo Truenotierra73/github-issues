08b40e56004a965853f54f88e209086bb4874716
Agustin Bollati <agustin.bollati@fluxit.com.ar>
2025-03-01 15:33:27 -0300

    feat: agregar servicio de labels

---
+++ src/app/modules/issues/services/labels.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+import {
+  CreateQueryResult,
+  injectQuery,
+} from '@tanstack/angular-query-experimental';
+
+import { getLabels } from '../actions';
+
+import { GitHubLabel } from '../interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LabelsService {
+  labelsQuery$: CreateQueryResult<GitHubLabel[], Error> = injectQuery(() => ({
+    queryKey: ['labels'],
+    queryFn: () => getLabels(),
+  }));
+}
