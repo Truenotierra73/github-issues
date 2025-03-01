7b724b4ddf3d9430fffb2a3f51c03ac01d32b268
Agustin Bollati <agustin.bollati@fluxit.com.ar>
2025-03-01 15:35:38 -0300

    feat: importar servicio para obtener los labels

--- src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
+++ src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
@@ -1,9 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { LabelsService } from '../../services/labels.service';
 
 @Component({
   selector: 'issues-list-page',
-  imports: [],
+  imports: [RouterLink, JsonPipe],
   templateUrl: './issues-list-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class IssuesListPageComponent {}
+export default class IssuesListPageComponent {
+  private readonly labelsService: LabelsService = inject(LabelsService);
+
+  get labels() {
+    return this.labelsService.labelsQuery$;
+  }
+}
