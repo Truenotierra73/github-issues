3130d5dc75bc515d79f0a0a96bd7929ad1109217
Agustin Bollati <agustin.bollati@fluxit.com.ar>
2025-03-01 21:01:45 -0300

    feat: agregar action para obtener las issues

---
+++ src/app/modules/issues/actions/get-issues.action.ts
@@ -0,0 +1,30 @@
+import { environment } from '@environments/environment';
+import { sleep } from '@helpers/sleep';
+
+import { GitHubIssue } from '../interfaces';
+
+const BASE_URL: string = environment.baseUrl;
+const GITHUB_TOKEN: string = environment.gitHubToken;
+
+export const getIssuesAction: () => Promise<GitHubIssue[]> = async (): Promise<
+  GitHubIssue[]
+> => {
+  try {
+    await sleep(1500);
+
+    const resp = await fetch(`${BASE_URL}/issues`, {
+      headers: {
+        Authorization: `Bearer ${GITHUB_TOKEN}`,
+      },
+    });
+
+    if (!resp.ok) {
+      throw new Error('Failed to get issues.');
+    }
+
+    return await resp.json();
+  } catch (ex) {
+    console.error(ex);
+    throw ex;
+  }
+};
