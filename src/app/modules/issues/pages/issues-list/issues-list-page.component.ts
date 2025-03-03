66d8dc5c4e3a447c3693f1f1aaf2003309f74e05
Agustin Bollati <agustin.bollati@fluxit.com.ar>
2025-03-02 23:38:25 -0300

    feat: mostrar lista de issues

--- src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
+++ src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
@@ -1,20 +1,26 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 import { IssuesLabelsSelectorComponent } from '../../components/issues-labels-selector/issues-labels-selector.component';
+import { IssueItemComponent } from '../../components/issue-item/issue-item.component';
 
 import { LabelsService } from '../../services/labels.service';
+import { IssuesService } from '../../services/issues.service';
 
 @Component({
   selector: 'issues-list-page',
-  imports: [RouterLink, IssuesLabelsSelectorComponent],
+  imports: [IssuesLabelsSelectorComponent, IssueItemComponent],
   templateUrl: './issues-list-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class IssuesListPageComponent {
   private readonly labelsService: LabelsService = inject(LabelsService);
+  private readonly issuesService: IssuesService = inject(IssuesService);
 
   get labels() {
     return this.labelsService.labelsQuery;
+  }
+
+  get issues() {
+    return this.issuesService.issuesQuery;
   }
 }
