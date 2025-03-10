9aa9c3abe3bb9eb87d5d51395a0cfcdb4628937d
Agustin Bollati <agustin.bollati@fluxit.com.ar>
2025-03-10 17:26:58 -0300

    test: modificar prueba de error

--- src/app/modules/issues/actions/get-issue-by-number.action.spec.ts
+++ src/app/modules/issues/actions/get-issue-by-number.action.spec.ts
@@ -29,7 +29,7 @@ describe('GetIssueByNumber [action]', () => {
     expect(result).toEqual(issue);
   });
 
-  it('should not fetch issue successfully', async () => {
+  it('should not fetch issue successfully', (done: DoneFn) => {
     const requestURL = `${BASE_URL}/issues/${issueNumber}`;
     const issueResponse = new Response(null, {
       status: 404,
@@ -38,13 +38,14 @@ describe('GetIssueByNumber [action]', () => {
 
     spyOn(window, 'fetch').and.resolveTo(issueResponse);
 
-    await getIssueByNumber(issueNumber).catch((error) => {
+    getIssueByNumber(issueNumber).catch((error) => {
       expect(window.fetch).toHaveBeenCalledWith(requestURL, {
         headers: {
           Authorization: `Bearer ${GITHUB_TOKEN}`,
         },
       });
       expect(error).toContain(`Error: Failed to get issue ${issueNumber}`);
+      done();
     });
   });
 });
